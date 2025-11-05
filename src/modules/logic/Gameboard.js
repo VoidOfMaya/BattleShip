afacc9a6b09e189d1f93442410adeb601777f2a1
VoidOfMaya <mayaofthevoid@gmail.com>
2025-11-05 10:30:02 +0200

    feat(logic):gameboard checks if all boats are sunk

--- src/modules/logic/Gameboard.js
+++ src/modules/logic/Gameboard.js
@@ -13,6 +13,26 @@ class Gameboard{
         this.grid[y][x]= new Ship(shipLength)
 
     }
+    //if coordinate is null meaning no ship,
+    // - set coordinate to false indicating the coordinate is no longer an option;
+    recieveAttack([x, y]){
+        const cell = this.grid[y][x];
+
+        if(cell === null) this.grid[y][x] = false;
+        else  if(cell instanceof Ship){
+            cell.hit(1);
+        }
+    }
+    //returns false if atleast one ship is still afloat returns true if all ships are sunk
+    allShipsSunk(){
+        let allSunk= true;
+        this.grid.forEach(row =>{
+            row.forEach(cell=>{
+                if(cell instanceof Ship && !cell.getIsSunk())allSunk = false ;
+            })
+        })
+        return allSunk;
+    }
 }
 export{
     Gameboard
