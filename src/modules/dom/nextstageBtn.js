1fb2046a8557d4aa60cc6e13015c67105f03cf9a
VoidOfMaya <mayaofthevoid@gmail.com>
2025-11-10 14:39:51 +0200

    style(in-button):center buttons

--- src/modules/dom/nextstageBtn.js
+++ src/modules/dom/nextstageBtn.js
@@ -2,8 +2,7 @@ const nextStage = document.createElement('div');
 nextStage.innerHTML = "move to next stage";
 nextStage.style.gridArea= "next";
 nextStage.className = "in-button";
-nextStage.style.alignSelf= "center";
-nextStage.style.justifySelf = "center";
+
 
 export {
     nextStage
