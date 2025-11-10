c84b3afba99f4f54f3906d85266fd2cafa50eac2
VoidOfMaya <mayaofthevoid@gmail.com>
2025-11-10 15:16:38 +0200

    refactor(ui): move fleet positioning game state to a separate js file

--- src/modules/fleetSetFlow.js
+++ src/modules/fleetSetFlow.js
@@ -1,61 +1,10 @@
-import { Player } from "./logic/Player";
-import { body, container, router } from "./dom/mainContainer";
-import { view as attackView} from "./dom/Attack";
-import { view as observeView} from "./dom/observe";
-import { view as initView} from "./dom/preInit";
 import { view as fleetSetView } from "./dom/setFleet";
-import { addEventListenerTocells } from "./fleetSetFlow";
+import { selectedShip, orientation} from "./dom/fleetSelect";
+import { nextStage } from "./dom/nextstageBtn";
 //import grids
 import { Ship } from "./logic/Ship";
 import { grid as settingGrid } from './dom/setFleet'
-
-/**
- * sudo game flow
- * check if game started
- * if not display in container a start game button
- * that will prompt  a p vs p or p vs Ai
- * on click display player board in ship placement mode!
- * on the bottom there will be a confirmation button to finalize ship placement,
- * on finalize on both or 1 player game mood switches to  play mode
- * cheks for first to sink apponent ships as winner
- * each player is aloted the follwoing:-
- *  name       -   cell length
- * -Carrier    - 5 cells
- * -battleship - 4 cells
- * -Cruiser    - 3 cells
- * -submarine  - 3 cells
- * -destroyer  - 2 cells
- */ 
-
-let currentMode = 0;
-const gameModes =[initView, fleetSetView, attackView, observeView];
-
-const statehandler=()=>{
-    
-    if(!container.hasChildNodes()){
-        gameModes.forEach(mode =>{
-            container.appendChild(mode);
-            mode.style.display = 'none'; 
-        });
-        gameModes[0].style.display = 'grid'; 
-    }
-    gameModes[currentMode].style.display = 'none';
-    currentMode = (currentMode + 1)% gameModes.length;
-    gameModes[currentMode].style.display= "grid";
-
-    
-}
-const gameState =()=>{
-    router.addEventListener('click', statehandler)
-}
-/*
 //ui handlers
-const addHoverPreview = (player)=>{
-    const cells =settingGrid.querySelectorAll('.cell');
-    cells.forEach(cell=>{
-
-    })
-}
 const syncGrid = (playerGrid )=>{
     gridSyncRester()
 
@@ -216,28 +165,7 @@ const resetPreview=()=>{
         }
     });
 }
-*/
 
-const battleship=()=>{
-    const mainPlayer = new Player();
-    const aiPlayer = new Player(true);
-    body
-    gameState();
-    addEventListenerTocells(mainPlayer);
-    
- 
-   // mainPlayer.gameboard.populateGrid([5,6], 4);
-    //mainPlayer.gameboard.populateGrid([1,1], 5);
-    //mainPlayer.gameboard.populateGrid([7,2], 3, "vertical");
-    //syncGrid(mainPlayer.gameboard.getGrid());
-    
-    
-
-}
-
-
-
-
-export {
-    battleship,
+export{
+    addEventListenerTocells,
 }
