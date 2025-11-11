053cb2d1e66fdaf0324a7f1039222a58e777ec69
VoidOfMaya <mayaofthevoid@gmail.com>
2025-11-11 15:47:19 +0200

    feat(battleStage):pushing unsaved changes due to end of day

--- src/modules/stages/battleStage.js
+++ src/modules/stages/battleStage.js
@@ -1,16 +1,30 @@
 import { view as attackView, grid} from "../dom/Attack";
 import { view as observeView} from "../dom/observe";
+import {container } from "../dom/mainContainer"
 import { nextStage } from "../fleetSetup";
 
 attackView.appendChild(nextStage);
 
-const battle=()=>{
-    let winner = false
-    console.log(grid)
-    while(!winner){
+const battle=(playerA,playerB, mode)=>{
 
+
+    console.log(grid)
+    if(mode === 'pvnpc'){
+        handlePvNpc();
     }
 
+}
+const handlePvNpc = async ()=>{
+    container.appendChild(attackView);
+    container.appendChild(observeView);
+    let stage = 'attack';
+    let winner = false;
+    while(!winner){
+        if(stage === 'attack'){
+            attackView.style.display = 'grid';
+            
+        }
+    }
 }
 
 export{
